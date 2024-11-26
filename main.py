5cc6542b23565411b51b4df811bc52ebe6e2691b
Alex-ux-dot <Alex03ed@gmail.com>
2024-11-26 10:05:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+A = 9
+B = 8 
+
+Addition = A + B
+subtraction = A-B
+Multiplication = A*B
+Division = A/B
+power = A**B
+
+print(Addition)
+print(subtraction)
+print(Multiplication)
+print(Division)
+print(power)
